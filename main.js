3a3a1e476cb1545bce3ff2057d447ad457cb3a2c
asoolito <roblox890king890@gmail.com>
2024-07-12 10:24:03 +0500

    ...

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
-const btnPlus = document.querySelector('.btn1')
-const btnMinus = document.querySelector('.btn2')
-const spanText = document.querySelector('.text')
+    // const todoForm = document.querySelector("form");
+    // const todoInput = document.querySelector(".input");
+    // const todoList = document.querySelector(".list");   
 
-let num = 0;
+    // todoForm.addEventListener("submit", (event) => {
+    //     event.preventDefault();
+    //     const inputValue = todoInput.value;
+    //     const newItem = document.createElement("li");
+    //     const todoCheckbox = document.createElement("input");
+    //     const todoEdit = document.createElement("button");
+    //     const todoDelete = document.createElement("button");})
 
-btnPlus.addEventListener('click', () => {
-  num++;
-  spanText.textContent = num;
-});
 
-btnMinus.addEventListener('click', () => {
-    num--;
-  spanText.textContent = num;
-})
+
+    const todoForm = document.querySelector(".form")
+const todoInput = document.querySelector(".input")
+const todoList = document.querySelector(".list")
+todoForm.addEventListener("submit", (said)=>{
+    said.preventDefault()
+    const inputValue = todoInput.value
+    const newItem = document.createElement("li")
+    const UserName = document.createElement("h2")
+    const todoCheckbox = document.createElement("input")
+    const todoEdit = document.createElement("button")
+    todoEdit.classList.add("Edit")
+    const todoDelete = document.createElement("button")
+    todoDelete.className = "Delete"
+    const wrapper  = document.createElement("div")
+
+    wrapper.append(todoDelete,todoEdit)
+    todoEdit.textContent = "edit"
+    todoDelete.textContent = "delete"
+    todoCheckbox.type = "checkbox"
+    todoCheckbox.className = "checkbox"
+    UserName.textContent = inputValue
+
+    newItem.className = "item"
+    document.body.appendChild(todoList)
+    const Username = document.createElement("h2")
+    Username.textContent = inputValue
+    newItem.append(todoCheckbox,todoDelete,todoEdit,Username)
+    todoList.appendChild(newItem)
+
+    todoDelete.addEventListener("click", (evt)=>{
+        todoList.removeChild(newItem)
+    })
+
+    todoInput.value = "";
+
+
+
+
+}) 
